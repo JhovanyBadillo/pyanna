81938ecd18202a176d41aa41b466884d7524cbd3
JhovanyBC <jhovany.badc@gmail.com>
2021-05-13 10:16:08 -0500

    Se generaliza signatura de función para recibir función lambda

--- fun/plot_prediccion.py
+++ fun/plot_prediccion.py
@@ -1,20 +1,27 @@
 import numpy
 import json
 from matplotlib import pyplot
-from typing import Dict, Any
+from typing import (
+    Dict,
+    Any,
+    Callable
+)
 
 
-def plot_prediccion(data_path: str, expr: str, filename: str, ext: str = 'svg'):
+def plot_prediccion(
+    data_path: str, expr: str, fn: Callable[[numpy.ndarray], numpy.ndarray], filename: str, ext: str = 'svg'
+):
     with open(data_path, "r") as f:
         data: Dict[str, Any] = json.load(f)
     
     x0: numpy.ndarray = numpy.array(data["x0"])
-    Z: numpy.ndarray = numpy.array(data["preds"])
+    Y: numpy.ndarray = numpy.array(data["Y"])
 
     fig = pyplot.figure()
-    pyplot.plot(x0[0,:], Z[0,:], 'g+', label='predicción')
-    pyplot.plot(sorted(x0[0,:]), numpy.exp(-numpy.array(sorted(x0[0,:]))), '-', color='b', label=expr)
+    pyplot.plot(x0[0,:], Y[0,:], 'g+', label='predicción')
+    x0 = numpy.array(sorted(x0[0,:]))
+    pyplot.plot(x0, fn(x0), '-', color='b', label=expr)
     pyplot.xlabel('x')
-    pyplot.ylabel('Z')
+    pyplot.ylabel('Y')
     pyplot.legend()
     pyplot.savefig(f"{filename}.{ext}")
